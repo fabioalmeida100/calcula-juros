f092c10d2bd93f9903e48239d3ccbca4c163ba19
Fábio Almeida <fabio_almeida100@hotmail.com>
2020-07-04 18:45:09 -0300

    Implementation of the unit tests

---
+++ calculajurosteste/Fakes/TaxaJurosZero.cs
@@ -0,0 +1,15 @@
+﻿using calculajuros.Lib;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace calculajurostest.Fakes
+{
+    public class TaxaJurosZero : ITaxaJuros
+    {
+        public double ObterTaxaJuros()
+        {
+            return 0;
+        }
+    }
+}
